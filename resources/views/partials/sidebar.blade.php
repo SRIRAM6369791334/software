ec3bb1574a7f70552424bf553a61e431ab39570f
SRIRAM <94049113+SRIRAM6369791334@users.noreply.github.com>
2026-04-29 18:20:56 +0530

    fix(sidebar): correct invalid route 'billing.create' to 'billing.daily.create'

--- resources/views/partials/sidebar.blade.php
+++ resources/views/partials/sidebar.blade.php
@@ -12,7 +12,7 @@
             ['label' => 'Vendors', 'route' => 'masters.vendors.index', 'icon' => '<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M20 7l-8-4-8 4m16 0l-8 4m8-4v10l-8 4m0-10L4 7m8 4v10M4 7v10l8 4" />'],
         ],
         'Billing' => [
-            ['label' => 'New Bill', 'route' => 'billing.create', 'icon' => '<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 4v16m8-8H4" />'],
+            ['label' => 'New Bill', 'route' => 'billing.daily.create', 'icon' => '<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 4v16m8-8H4" />'],
             ['label' => 'Weekly Billing', 'route' => 'billing.weekly.index', 'icon' => '<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 12h6m-6 4h6m2 5H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z" />'],
             ['label' => 'Daily Billing', 'route' => 'billing.daily.index', 'icon' => '<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z" />'],
         ],
